9f0a96a22b691773b9889253931e8bee1f9d42a8
Oleksandra Vytiahlovska <lesyalp@gmail.com>
2021-02-11 15:56:09 +0100

    refactore to use absolute path or aliases in imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extenstions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js', '.json'],
+        alias: {
+          '@navigation': './src/navigation',
+          '@atoms': './src/components/atoms',
+          '@molecules': './src/components/molecules',
+          '@organisms': './src/components/organisms',
+          '@templates': './src/components/templates',
+          '@pages': './src/components/pages',
+          '@gql': './src/graphql',
+          '@app-types': './src/types',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import { ApolloProvider } from '@apollo/client';
 
-import App from './App';
+import AppWrapper from '@templates/AppWrapper';
 import { name as appName } from './app.json';
-import client from './src/graphQL/client';
-
-const AppWrapper = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
