5668a6b726dad69d07e6b39e9374fdb21cb8da09
Oleksandra Vytiahlovska <lesyalp@gmail.com>
2021-02-12 11:24:44 +0100

    add StartScreen component, add asset folder and background image

---
+++ src/components/pages/StartScreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import background from '@src/assets/img/world.jpg';
+import Header from '@atoms/Header';
+
+const StartScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <ImageBackground source={background} style={styles.backgroundImage}>
+      <Header />
+      <TouchableOpacity>
+        <Text>rules</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>15 questions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>20 questions</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>30 questions</Text>
+      </TouchableOpacity>
+    </ImageBackground>
+  </SafeAreaView>
+);
+
+export default StartScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'yellow',
+    height: '100%',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+});
--- src/components/templates/AppWrapper.tsx
+++ src/components/templates/AppWrapper.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { ApolloProvider } from '@apollo/client';
 import client from '@gql/client';
-import TestComponent from '@atoms/Test';
+import StartScreen from '@pages/StartScreen';
 
 const AppWrapper: React.FC = (): JSX.Element => {
   return (
     <ApolloProvider client={client}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <TestComponent />
+        <StartScreen />
       </SafeAreaView>
     </ApolloProvider>
   );
